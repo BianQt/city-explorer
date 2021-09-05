6d531707da75eb2045df0711cfcc1de44333717a
BianQt <bayanqutshan@gmail.com>
2021-09-05 23:20:42 +0300

    Almost Done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Card, Button } from "react-bootstrap";
 
 class Footer extends React.Component {
-
-    render(){
-        return <footer>
-            <h1>footer</h1>
-        </footer>
-    }
-    
-
-
-
-
+  render() {
+    return (
+      <footer>
+        <Card className="text-center">
+          <Card.Footer className="text-muted footer">©{new Date().getFullYear()} City Explorer</Card.Footer>
+        </Card>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
