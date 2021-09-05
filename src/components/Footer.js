b2e77b5b090ea15a20c38a1e9407b42310c94567
BianQt <bayanqutshan@gmail.com>
2021-09-05 15:27:13 +0300

    Kickstart Files

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Footer extends React.Component {
+
+    render(){
+        return <footer>
+            <h1>footer</h1>
+        </footer>
+    }
+    
+
+
+
+
+}
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class Main extends React.Component {
+
+
+
+
+
+
+render(){
+    return <main>
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+    </main>
+}
+}
+
+export default Main;
