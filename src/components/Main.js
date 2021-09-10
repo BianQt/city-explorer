6852a6f13a5f55a889372c03d1ec7b4fa760e51e
BianQt <bayanqutshan@gmail.com>
2021-09-11 02:12:31 +0300

    lab10

--- src/components/Main.js
+++ src/components/Main.js
@@ -24,7 +24,7 @@ class Main extends React.Component {
       displayName: "",
       show: false,
       errShow: false,
-      weatherData: {},
+      weatherData: [],
       moviesData: [],
     };
   }
@@ -38,24 +38,35 @@ class Main extends React.Component {
     e.preventDefault();
     const apiKey = process.env.REACT_APP_LOCATION_API_KEY;
     const url = `https://us1.locationiq.com/v1/search.php?key=${apiKey}&city=${this.state.cityName}&format=json`;
-    const weatherUrl = `${process.env.REACT_APP_LOCAL_SERVER_API}/weather?q=${this.state.cityName}`;
+   
     const moviesUrl = `${process.env.REACT_APP_LOCAL_SERVER_API}/movies?q=${this.state.cityName}`;
-    console.log("env", process.env);
+
     try {
-      const locationData = await axios.get(url);
-      const weatherData = await axios.get(weatherUrl);
-      const moviesData = await axios.get(moviesUrl);
-      const imgUrl = `https://maps.locationiq.com/v3/staticmap?key=${apiKey}&center=${locationData.data[0].lat},${locationData.data[0].lon}&zoom=14&size=450x300&markers=icon:large-red-cutout|${locationData.data[0].lat},${locationData.data[0].lon}`;
-      console.log("movies", moviesData.data);
+      axios.get(url).then((locationData) => {
+        const imgUrl = `https://maps.locationiq.com/v3/staticmap?key=${apiKey}&center=${locationData.data[0].lat},${locationData.data[0].lon}&zoom=14&size=450x300&markers=icon:large-red-cutout|${locationData.data[0].lat},${locationData.data[0].lon}`;
+        this.setState({
+          lat: locationData.data[0].lat,
+          lon: locationData.data[0].lon,
+          imgUrl: imgUrl,
+          displayName: locationData.data[0].display_name,
+        });
+        const weatherUrl = `${process.env.REACT_APP_LOCAL_SERVER_API}/weather?lat=${locationData.data[0].lat}&lon=${locationData.data[0].lon}`;
+        axios.get(weatherUrl).then((weatherData) => {
+          this.setState({
+            weatherData: weatherData.data,
+          });
+        });
+      });
+
+      axios.get(moviesUrl).then((moviesData) => {
+        this.setState({
+          moviesData: moviesData.data,
+        });
+      });
+
       this.setState({
-        lat: locationData.data[0].lat,
-        lon: locationData.data[0].lon,
-        imgUrl: imgUrl,
-        displayName: locationData.data[0].display_name,
         show: true,
         errShow: false,
-        weatherData: weatherData.data,
-        moviesData: moviesData.data,
       });
     } catch (e) {
       console.log("Something went wrong.");
@@ -72,51 +83,50 @@ class Main extends React.Component {
         <Container>
           <Row className="row-grid align-items">
             <Col md="6">
-            <Form className="form" onSubmit={this.submission}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>
-            <h3>Find Your Next Destination!</h3>
-            </Form.Label>
-            <Form.Control
-            type ="text"
-            placeholder="Enter City Name"
-            onChange={this.onChange}
-            />
-            <Form.Text className="text-muted"></Form.Text>
-            </Form.Group>
+              <Form className="form" onSubmit={this.submission}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>
+                    <h3>Find Your Next Destination!</h3>
+                  </Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter City Name"
+                    onChange={this.onChange}
+                  />
+                  <Form.Text className="text-muted"></Form.Text>
+                </Form.Group>
 
-            <Button className="button" variant="danger" type ="submit">
-            Find
-            </Button>
-            </Form>
-          {this.state.show && (<div>
-
-            <Table striped bordered hover>
-            <tbody>
-            <tr>
-            <td>City</td>
-            <td>{this.state.displayName}</td>
-            </tr>
-            <tr>
-            <td>Latitude</td>
-            <td>{this.state.lat}</td>
-            </tr>
-            <tr>
-            <td>Longtitude</td>
-            <td>{this.state.lon}</td>
-            </tr>
-            </tbody>
-            </Table>
-            <img className="map" src={this.state.imgUrl} />
-            
-            </div>)}
+                <Button className="button" variant="danger" type="submit">
+                  Find
+                </Button>
+              </Form>
+              {this.state.show && (
+                <div>
+                  <Table striped bordered hover>
+                    <tbody>
+                      <tr>
+                        <td>City</td>
+                        <td>{this.state.displayName}</td>
+                      </tr>
+                      <tr>
+                        <td>Latitude</td>
+                        <td>{this.state.lat}</td>
+                      </tr>
+                      <tr>
+                        <td>Longtitude</td>
+                        <td>{this.state.lon}</td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                  <img className="map" src={this.state.imgUrl} />
+                </div>
+              )}
             </Col>
-          {this.state.show &&<Col md="6" style={{ visibility: this.state.show }}>
-            <Weather weatherData={this.state.weatherData} />
-
-
-            </Col>}
-
+            {this.state.show && (
+              <Col md="6" style={{ visibility: this.state.show }}>
+                <Weather weatherData={this.state.weatherData} />
+              </Col>
+            )}
           </Row>
           {this.state.show && <Movies moviesList={this.state.moviesData} />}
           {this.state.errShow && (
